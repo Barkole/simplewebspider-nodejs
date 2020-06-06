28f043ac1403d0198f654aba1783e0528310ea5d
Michael D <2873958-Lepardo@users.noreply.gitlab.com>
2020-06-06 23:00:45 +0200

    Setup project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ["standard"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    // Make sure that plugin:prettier/recommended is the last configuration in the extends array
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
--- README.md
+++ README.md
@@ -5,12 +5,13 @@
 1. Install [VSCode](https://code.visualstudio.com/)
 2. Install [Git Bash for Windows](https://gitforwindows.org/)
 3. Setup your git
-   1. `git config --global user.name ""`
+   1. `git config --global user.name "foo"`
    2. `git config --global user.email "email@example.com"`
 4. Install [node.js 12.x](https://nodejs.org/)
 5. Install [yarn](https://yarnpkg.com/) - [Installation](https://classic.yarnpkg.com/en/docs/install/#windows-stable)
-6. Clone project: `git clone ...`
-7. Setup VSCode
+6. Install [lint-staged](https://github.com/okonet/lint-staged): `npx mrm lint-staged`
+7. Clone project: `git clone ...`
+8. Setup VSCode
    1. Install recommended extensions
    2. Install all dependencies: `yarn install`
 
