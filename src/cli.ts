23738a4037f98743bf70035d01a3c7eedc367015
Michael D <2873958-Lepardo@users.noreply.gitlab.com>
2020-06-07 16:09:12 +0200

    Test wrapped exception logging

--- src/cli.ts
+++ src/cli.ts
@@ -5,16 +5,35 @@ process.on(`uncaughtException`, function (err) {
   logger.error(`Uncaught error`, err);
 });
 
+class SimpleWrapError extends Error {
+  cause: Error | undefined;
+  details: string[] | undefined;
+  constructor(message?: string, details?: string[], cause?: Error) {
+    super(message);
+    this.name = `SimpleWrapError`;
+    this.details = details;
+    this.cause = cause;
+  }
+}
+
 setTimeout(() => {
-  throw new Error(`Hacky`);
+  throw new SimpleWrapError(
+    `Wrapped2`,
+    [`Detail 3`, `Detail 4`],
+    new SimpleWrapError(
+      `Wrapped1`,
+      [`Detail 1`, `Detail 2`],
+      new Error(`Hacky`)
+    )
+  );
 }, 0);
 
 process.nextTick(() => {
   throw new Error(`Hacky`);
 });
 
-logger.info(`Build ${JSON.stringify(config.build)}`);
-logger.debug(`Configuration: ${JSON.stringify(config)}`);
+logger.info(`Build`, config.build);
+logger.debug(`Configuration`, config);
 
 const message = `Hello World ${config.username}:\nTEST: ${config.test}`;
 logger.info(message);
