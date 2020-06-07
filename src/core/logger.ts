103d7e55f83e4b69d05a4c1f5c296973a95edca3
Michael D <2873958-Lepardo@users.noreply.gitlab.com>
2020-06-07 16:12:36 +0200

    Test jsoon wrapped exception logging

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,39 +1,42 @@
-import { createLogger, format, transports } from "winston";
-import { consoleFormat } from "winston-console-format";
+import winston from "winston";
 import { config } from "./config";
 
-// export const logger = winston.createLogger({
-//   levels: winston.config.cli.levels,
-//   transports: [new winston.transports.Console({ level: config.log.level })],
-// });
-
-export const logger = createLogger({
-  level: config.log.level,
-  format: format.combine(
-    format.timestamp(),
-    format.ms(),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: { service: `Test` },
-  transports: [
-    new transports.Console({
-      format: format.combine(
-        format.colorize({ all: true }),
-        format.padLevels(),
-        consoleFormat({
-          showMeta: true,
-          metaStrip: [`service`],
-          inspectOptions: {
-            depth: Infinity,
-            colors: true,
-            maxArrayLength: Infinity,
-            breakLength: 120,
-            compact: Infinity,
-          },
-        })
-      ),
-    }),
-  ],
+export const logger = winston.createLogger({
+  levels: winston.config.cli.levels,
+  transports: [new winston.transports.Console({ level: config.log.level })],
 });
+
+// import { createLogger, format, transports } from "winston";
+// import { consoleFormat } from "winston-console-format";
+// import { config } from "./config";
+
+// export const logger = createLogger({
+//   level: config.log.level,
+//   format: format.combine(
+//     format.timestamp(),
+//     format.ms(),
+//     format.errors({ stack: true }),
+//     format.splat(),
+//     format.json()
+//   ),
+//   defaultMeta: { service: `Test` },
+//   transports: [
+//     new transports.Console({
+//       format: format.combine(
+//         format.colorize({ all: true }),
+//         format.padLevels(),
+//         consoleFormat({
+//           showMeta: true,
+//           metaStrip: [`service`],
+//           inspectOptions: {
+//             depth: Infinity,
+//             colors: true,
+//             maxArrayLength: Infinity,
+//             breakLength: 120,
+//             compact: Infinity,
+//           },
+//         })
+//       ),
+//     }),
+//   ],
+// });
