f5d19d0b6ee45dc63aa0e5190c42d1895b79d572
Michael D <2873958-Lepardo@users.noreply.gitlab.com>
2020-06-12 21:19:15 +0200

    Remove outcommented test code

--- src/cli.ts
+++ src/cli.ts
@@ -9,56 +9,6 @@ process.on(`uncaughtException`, function (err) {
   logger.error(`\n##\nUncaught error`, err);
 });
 
-// class SimpleWrapError extends Error {
-//   cause: Error | undefined;
-//   details: string[] | undefined;
-//   constructor(message?: string, details?: string[], cause?: Error) {
-//     super(message);
-//     this.name = `SimpleWrapError`;
-//     this.details = details;
-//     this.cause = cause;
-//   }
-// }
-
-// setTimeout(() => {
-//   const error1 = new SimpleWrapError(
-//     `Wrapped1`,
-//     [`Detail 1`, `Detail 2`],
-//     new Error(`Hacky`)
-//   );
-//   const error2 = new SimpleWrapError(
-//     `Wrapped2`,
-//     [`Detail 3`, `Detail 4`],
-//     error1
-//   );
-//   throw error2;
-// }, 0);
-
-// class MyErrlop extends Errlop {
-//   details: string[] | undefined;
-//   constructor(message: string, details?: string[], cause?: Error) {
-//     super(message, cause);
-//     this.name = `MyErrlop`;
-//     this.details = details;
-//   }
-// }
-
-// setTimeout(() => {
-//   const error1 = new Error(`MyErrlop0`);
-//   const error2 = new MyErrlop(`MyErrlop1`, [`Detail 1`, `Detail 2`], error1);
-//   const error3 = new MyErrlop(`MyErrlop2`, [`Detail 3`, `Detail 4`], error2);
-//   throw error3;
-// }, 0);
-
-// process.nextTick(() => {
-//   throw new Error(`Hacky`);
-// });
-
-// process.nextTick(() => {
-//   const error1 = new Errlop(`Errlop21`);
-//   const error2 = new Errlop(`Errlop2`, error1);
-//   throw error2;
-// });
 logger.info(`Build`, config.build);
 logger.debug(`Configuration`, config);
 
