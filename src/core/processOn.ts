834126b709a52b504358287acdd4f1713a31442e
Michael D <2873958-Lepardo@users.noreply.gitlab.com>
2020-06-12 22:19:46 +0200

    improve process on logging

--- src/core/processOn.ts
+++ src/core/processOn.ts
@@ -14,8 +14,8 @@ function processOn(): void {
   });
   process.on(`exit`, (code) => {
     // eslint-disable-next-line no-console
-    console.warn(`Exit`, code);
-    logger.warn(`Exit`, code);
+    console.warn(`Exit ${code}`);
+    logger.warn(`Exit ${code}`);
   });
   process.on(`uncaughtException`, function (err) {
     // eslint-disable-next-line no-console
@@ -29,8 +29,8 @@ function processOn(): void {
   });
   process.on(`unhandledRejection`, (reason) => {
     // eslint-disable-next-line no-console
-    console.warn(`Unhandled Rejection`, reason);
-    logger.warn(`Unhandled Rejection`, reason);
+    console.warn(`Unhandled Rejection ${JSON.stringify(reason)}`);
+    logger.warn(`Unhandled Rejection ${JSON.stringify(reason)}`);
   });
   process.on(`warning`, (warning) => {
     // eslint-disable-next-line no-console
@@ -39,53 +39,53 @@ function processOn(): void {
   });
   process.on(`message`, (message) => {
     // eslint-disable-next-line no-console
-    console.warn(`Message`, message);
-    logger.warn(`Message`, message);
+    console.warn(`Message ${JSON.stringify(message)}`);
+    logger.warn(`Message ${JSON.stringify(message)}`);
   });
   process.on(`SIGABRT`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGABRT`, signal);
-    logger.warn(`SIGABRT`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGHUP`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGHUP`, signal);
-    logger.warn(`SIGHUP`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGINT`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGINT`, signal);
-    logger.warn(`SIGINT`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGKILL`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGKILL`, signal);
-    logger.warn(`SIGKILL`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGQUIT`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGQUIT`, signal);
-    logger.warn(`SIGQUIT`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGSTOP`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGSTOP`, signal);
-    logger.warn(`SIGSTOP`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGUSR1`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGUSR1`, signal);
-    logger.warn(`SIGUSR1`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGUSR2`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGUSR2`, signal);
-    logger.warn(`SIGUSR2`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
   process.on(`SIGBREAK`, (signal) => {
     // eslint-disable-next-line no-console
-    console.warn(`SIGBREAK`, signal);
-    logger.warn(`SIGBREAK`, signal);
+    console.warn(`${signal}`);
+    logger.warn(`${signal}`);
   });
 }
 
