d43d0dcebe70a8352c19fee0481d7a48fb19c637
Michael D <2873958-Lepardo@users.noreply.gitlab.com>
2020-06-15 12:52:51 +0200

    Use p-any for Promise queue to speed up processing

--- src/service/extractor/SimpleExtractor.ts
+++ src/service/extractor/SimpleExtractor.ts
@@ -1,4 +1,5 @@
 import { IExtractor } from "./IExtractor";
+import { randomInt } from "../../core/utils";
 
 function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -7,7 +8,7 @@ function sleep(ms: number): Promise<void> {
 export class SimpleExctractor implements IExtractor {
   async extract(url: string): Promise<string[]> {
     // TODO Implement this
-    await sleep(10000);
+    await sleep(randomInt(1000, 10000));
     return [url];
   }
 }
--- src/service/queue/PromiseQueue.ts
+++ src/service/queue/PromiseQueue.ts
@@ -4,6 +4,7 @@ import { checkValidateSync } from "../../core/utils";
 import { logger } from "../../core/logger";
 import { v4 as uuid } from "uuid";
 import { IQueueConfig } from "./IQueueConfig";
+import pAny from "p-any";
 
 export class PromiseQueue implements IQueue {
   @IsInt()
@@ -16,25 +17,27 @@ export class PromiseQueue implements IQueue {
   async add(func: (this: void) => Promise<void>): Promise<void> {
     const id = uuid();
     logger.silly(`Enqueue [id=${id}, size=${this.queue.size}]`);
-    this.queue.set(
-      id,
-      (async (queue: Map<string, unknown>, id: string): Promise<void> => {
-        logger.silly(`Starting [id=${id}, size=${this.queue.size}]`);
-        try {
-          await func();
-        } catch (e) {
-          logger.warn(`Execution failed ${id}`, e);
-        } finally {
-          logger.silly(`Removing [id=${id}, size=${this.queue.size}]`);
-          queue.delete(id);
-        }
-      })(this.queue, id)
-    );
+    this.queue.set(id, this.execute(id, func));
+
     if (this.queue.size >= this.parallel) {
       logger.silly(`Waiting... [size=${this.queue.size}]`);
-      // We would need Promise.any()
-      await Promise.all(this.queue.values());
+      await pAny(this.queue.values());
       logger.silly(`Continue... [size=${this.queue.size}]`);
+    }
+  }
+
+  async execute(
+    id: string,
+    func: (this: void) => Promise<void>
+  ): Promise<void> {
+    logger.silly(`Starting [id=${id}, size=${this.queue.size}]`);
+    try {
+      await func();
+    } catch (e) {
+      logger.warn(`Execution failed ${id}`, e);
+    } finally {
+      logger.silly(`Removing [id=${id}, size=${this.queue.size}]`);
+      this.queue.delete(id);
     }
   }
 
